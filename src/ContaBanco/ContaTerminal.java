40100ae4fb780e113a75453dc182593d86f3741b
Lucas <dev.pac.mcz@gmail.com>
2024-09-27 09:37:27 -0300

    Meu projeto conta banco

---
+++ src/ContaBanco/ContaTerminal.java
@@ -0,0 +1,17 @@
+package ContaBanco;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        String nome;
+        int numeroAgencia;
+        int numeroConta;
+        double saldo;
+
+
+
+
+
+    }
+
+
+}
